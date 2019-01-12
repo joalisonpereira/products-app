127c6829ba4ef2480f37c4e64e12b2a70a41eea4
Joalison Pereira <joalison.pereira@hotmail.com>
2019-01-12 12:16:11 -0200

    Connect react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import Home from './pages/Home';
+import Router from './routes';
 import store from './store';
 
 const App = () => (
   <Provider store={store}>
-    <Home/>
+    <Router/>
   </Provider>
 );
 
