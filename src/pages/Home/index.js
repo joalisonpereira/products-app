6f66d729ee4c8e3457faa4ce2895f123492874b9
Joalison Pereira <joalison.pereira@hotmail.com>
2019-01-13 05:10:37 -0200

    Tests to blob fields

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,48 +1,60 @@
 import React, { Component } from 'react';
-import imageToBlob from 'image-to-blob';
 
 import { api } from '../../services';
+import { b64toBlob } from '../../utils';
 import Style from './styles';
 
 class Home extends Component {
-  
+
   state = {
-  	src : '',
-  	src2 : ''
+  	name: 'asdadasd',
+  	price: 300,
+  	describe: 'asdasda'
   }
 
-  _onChangeImage(e){
-  	let { files } = e.target;
-  	let reader = new FileReader();
-    reader.addEventListener("load", e => {
-		this.setState({
-			src: e.target.result
-		})
-    });	
-    reader.readAsDataURL(files[0]);
+  async _handleSubmit(e){
+  	e.preventDefault();
+	let photo = document.querySelector("#photo").files[0];
+	let formData = new FormData();
+	formData.append('name',this.state.name);
+	formData.append('price',this.state.price)
+	formData.append('describe',this.state.price);
+	formData.append('photo', photo);
+	try{
+		const response = await api.post('/products',formData);
+		console.log(response);
+		return
+	}catch(error){
+		console.log(error.response);
+	}
   }
 
-  _consoleBlob(){
-  	let img = document.querySelector("#img1");
-  	let DOMURL = window.URL || window.webkitURL || window;
-  	imageToBlob(img,(error,blob) => {
-  		if(error) return;
-		this.setState({
-			src2: DOMURL.createObjectURL(blob)
-		})
-  	});
+  async loadImage(){
+	const response = await api.get('/products/5');
+	const DOMURL = window.URL || window.webkitURL || window;
+	let { photo,photo_type } = response.data.data;
+	photo = b64toBlob(photo,photo_type);
+	document.querySelector("img").src = DOMURL.createObjectURL(photo);
   }
 
   render() {
     return (
       <Style>
-      	<h1>Home</h1>
-      	<img src={this.state.src} id="img1" style={{width:300}}/>
-      	<img src={this.state.src2} id="img2" style={{width:200}}/>
-      	<input type="file" onChange={e => this._onChangeImage(e)}/>
+      	<form onSubmit={e => this._handleSubmit(e)} encType="multipart/form-data">
+			<input type="text" onChange={e => {}} value={this.state.name}/>
+			<br/><br/>
+			<input type="number" onChange={e => {}} value={this.state.price}/>
+			<br/><br/>
+			<input type="text" onChange={e => {}} value={this.state.describe}/>
+			<br/><br/>
+			<input type="file" name="photo" id="photo"/>
+			<br/><br/>
+			<button>Enviar</button>
+      	</form>
       	<br/><br/>
-      	<button onClick={() => this._consoleBlob()}>
-      		Get blob
+      	<img src=""/>
+      	<button onClick={() => this.loadImage()}>
+      		Carregar imagem
       	</button>
       </Style>
     );
--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 export const api = axios.create({
-	baseURL: 'http://localhost/products-api/public/api'
+	baseURL: 'http://localhost/products-api/public/api',
+	headers: {
+		'Content-Type': 'multipart/form-data',
+	}
 });
 
